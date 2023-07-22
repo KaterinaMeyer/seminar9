38e06c7afd0b406b71bd85a00dd0378926edbca5
Катерина <katttum@ya.ru>
2023-07-22 18:40:39 +0300

    сумма чисел в промежутке

---
+++ task2_66/Program.cs
@@ -0,0 +1,29 @@
+﻿//Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+//M = 1; N = 15 -> 120
+//M = 4; N = 8. -> 30
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите значение M: ");
+        int m = int.Parse(Console.ReadLine());
+
+        Console.Write("Введите значение N: ");
+        int n = int.Parse(Console.ReadLine());
+
+        int sum = CalculateSum(m, n);
+
+        Console.WriteLine($"Сумма натуральных элементов в промежутке от {m} до {n}: {sum}");
+    }
+
+    static int CalculateSum(int m, int n)
+    {
+        int sum = 0;
+        for (int i = m; i <= n; i++)
+        {
+            sum += i;
+        }
+        return sum;
+    }
+}
